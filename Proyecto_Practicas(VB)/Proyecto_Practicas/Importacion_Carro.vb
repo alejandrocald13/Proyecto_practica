6e9cb996105256177b8a13e8bc16a77ac07d5431
alejandrocald13 <robertoalejandrocalderon@gmail.com>
2022-09-05 09:29:04 -0600

    Commit 13 (05/09)
    Se conectó a la BD en el form de Importaciones.

--- Proyecto_Practicas(VB)/Proyecto_Practicas/Importacion_Carro.vb
+++ Proyecto_Practicas(VB)/Proyecto_Practicas/Importacion_Carro.vb
@@ -1,3 +1,19 @@
-﻿Public Class Importacion_Carro
-
+﻿Imports MySql.Data.MySqlClient
+Public Class Importacion_Carro
+    Dim cmd As MySqlCommand
+    Dim conn As New MySqlConnection
+    Dim objetoconexion As New conexion
+    Sub mostrar()
+        conn = objetoconexion.AbrirCon
+        Dim query As String = "SELECT i.id_impo as 'ID', ifc.vin_carro as 'VIN', cc.lote_compra as 'Lote de Compra', i.encarg_impo as 'Encargado de Importación', i.metodo_impo as 'Método de Importación', i.costo_impo as 'Costo de Importación' FROM importaciones i INNER JOIN info_carro ifc ON i.id_carro = ifc.id_carro INNER JOIN compra_carros cc ON cc.id_compras =i.id_compras;"
+        Dim adpt As New MySqlDataAdapter(query, conn)
+        Dim ds As New DataSet()
+        adpt.Fill(ds)
+        dgvCompraC.DataSource = ds.Tables(0)
+        conn.Close()
+        conn.Dispose()
+    End Sub
+    Private Sub Importacion_Carro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        mostrar()
+    End Sub
 End Class
